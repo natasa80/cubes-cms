7842a3509e5bbeccfb3f2b82e0958cb090e460cf
Natasa Lukic <natasa80lukic@gmail.com>
2016-07-17 21:53:06 +0200

    Domaci 17.07. new1

--- application/models/DbTable/CmsMembers.php
+++ application/models/DbTable/CmsMembers.php
@@ -28,8 +28,6 @@ class Application_Model_DbTable_CmsMembers extends Zend_Db_Table_Abstract {
         }
     }
 
-    
-    
     public function updateMember($id, $member) {
 
         //izbegavamo da se promeni id usera, brise se iz niza ukoliko je setovan
@@ -39,72 +37,127 @@ class Application_Model_DbTable_CmsMembers extends Zend_Db_Table_Abstract {
         }
         $this->update($member, 'id = ' . $id);
     }
-    
-    
-    
+
     /**
      * 
      * @param array $member
      * @return int The new ID of new member (autoincrement)
      */
-    public function insertMember($member){
-        //fetch order number for new member
-        
+//    public function insertMember($member){
+//        //fetch order number for new member
+//        
+//        $id = $this->insert($member);
+//        
+//        return $id;
+//       
+//    }
+
+
+
+    public function insertMember($member) {
+
+        $maxOrderNumber = new Zend_Db_Expr('MAX(order_number) as order_number');
+
+        $select = $this->select()
+                ->from(array(
+            'cms_members' => 'cms_members'), $maxOrderNumber);
+
+
+        $row = $this->fetchRow($select);
+
+        if (empty($row)) {
+
+            return FALSE;
+        } else {
+            $rowData = $row->toArray();
+        }
+
+        $newOrderNumber = $rowData['order_number'] + 1;
+
+        $member[order_number] = $newOrderNumber;
         $id = $this->insert($member);
-        
+
         return $id;
-        
-        
-        
     }
-    
+
     /**
      * 
      * @param int $id ID of member to delete
      */
-    public function deleteMember($id){
-        
-        $this->delete('id = ' . $id );
-        
-        
-    }
-    
-    /**
-     * 
-     * @param nt $id ID of member to enable
-     */
-    public function disableMember($id){
-        
-        $this->update(array(
-            'status'=>  self::STATUS_DISABLED
-        ),'id = ' .$id);
-    }
-    
-    /**
-     * 
-     * @param nt $id ID of member to enable
-     */
-     public function enableMember($id){
-        
-        $this->update(array(
-            'status'=>  self::STATUS_ENABLED
-        ),'id = ' .$id);
-    }
-    
-    
-    public function updateMemberOfOrder($sortedIds){
-        
-        foreach ($sortedIds as $orderNumber => $id){
-            
-             $this->update(array(
-            'order_number'=>  $orderNumber + 1 // +1 because it starts from 0
-        ),'id = ' .$id);
-             
-             
+    public function deleteMember($id) {
+
+        //dobijamo zeljeni red sa ID-jem koji se brise
+        $select = $this->select();
+        $select->where('id =?', $id);
+
+        $row = $this->fetchRow($select);
+
+        if ($row instanceof Zend_Db_Table_Row) {
+
+            $row->toArray();
+        } else {
+            return null;
         }
-        
-        
+
+        //dobijamo order number od ID-ja koji se brise
+        $orderDeleted = $row['order_number'];
+        // print_r($orderDeleted);
+
+        $select = $this->select();
+        $select->where('order_number > ?', $orderDeleted);
+
+        //dobijamo sve kolone koje zadovoljavaju uslov
+        $rows = $this->fetchAll($select);
+        // print_r($rows);
+
+        if (empty($rows)) {
+            return FALSE;
+        } else {
+            $rowsData = $rows->toArray();
+        }
+        // print_r($rowsData);
+
+
+        foreach ($rowsData as $row) {
+
+            $this->update(array(
+                'order_number' => $row['order_number'] - 1
+                    ), 'id= ' . $row['id']);
+        }
+
+        $this->delete('id = ' . $id);
     }
-    
+
+    /**
+     * 
+     * @param nt $id ID of member to enable
+     */
+    public function disableMember($id) {
+
+        $this->update(array(
+            'status' => self::STATUS_DISABLED
+                ), 'id = ' . $id);
+    }
+
+    /**
+     * 
+     * @param nt $id ID of member to enable
+     */
+    public function enableMember($id) {
+
+        $this->update(array(
+            'status' => self::STATUS_ENABLED
+                ), 'id = ' . $id);
+    }
+
+    public function updateMemberOfOrder($sortedIds) {
+
+        foreach ($sortedIds as $orderNumber => $id) {
+
+            $this->update(array(
+                'order_number' => $orderNumber + 1 // +1 because it starts from 0
+                    ), 'id = ' . $id);
+        }
+    }
 
 }
--- application/models/DbTable/CmsServices.php
+++ application/models/DbTable/CmsServices.php
@@ -37,21 +37,79 @@ class Application_Model_DbTable_CmsServices extends Zend_Db_Table_Abstract {
         $this->update($user, 'id = ' . $id);
     }
 
-    public function insertService($service) {
-        $id = $this->insert($service);
 
-        return $id;
+
+    
+    
+      public function insertService($service){
+        
+        $maxOrderNumber = new Zend_Db_Expr('MAX(order_number) as order_number');
+        $select = $this->select()
+                    ->from(array(
+                        'cms_services' => 'cms_services'),$maxOrderNumber );
+        
+        $row = $this->fetchRow($select);
+        
+       if(empty($row)){
+           
+           return FALSE;
+       }else{
+           $rowData = $row->toArray();
+       }
+       
+       $newOrderNumber = $rowData['order_number']+1;
+       
+       $service[order_number] = $newOrderNumber;
+       $id = $this->insert($service);
+       
+       return $id;
+       
+        
     }
-
     /**
      * 
      * @param int $id ID of member to delete
      */
-    public function deleteService($id) {
+   public function deleteService($id) {
+
+        //dobijamo zeljeni red sa ID-jem koji se brise
+        $select = $this->select();
+        $select->where('id =?', $id);
+
+        $row = $this->fetchRow($select);
+
+        if ($row instanceof Zend_Db_Table_Row) {
+
+            $row->toArray();
+        } else {
+            return null;
+        }
+
+        //dobijamo order number od ID-ja koji se brise
+        $orderDeleted = $row['order_number'];
+       
+
+        $select = $this->select();
+        $select->where('order_number > ?', $orderDeleted);
+
+        //dobijamo sve kolone koje zadovoljavaju uslov
+        $rows = $this->fetchAll($select);
+        
+        if (empty($rows)) {
+            return FALSE;
+        } else {
+            $rowsData = $rows->toArray();
+        }
+       
+        foreach ($rowsData as $row) {
+
+            $this->update(array(
+                'order_number' => $row['order_number'] - 1
+                    ), 'id= ' . $row['id']);
+        }
 
         $this->delete('id = ' . $id);
     }
-
     /**
      * 
      * @param nt $id ID of member to enable
